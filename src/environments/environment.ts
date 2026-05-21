507e1ba7f8f1968b3ea6ec2e7091e927d3e62aa7
DanteRodriguezHerrera <papikra222@gmail.com>
2026-05-21 13:51:20 -0600

    Fix redirect_uri for login twitch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.streamersleveling.com',
-    redirect_uri: 'https://streamersleveling.com'
+    redirect_uri: 'https://streamersleveling.com/login'
 };
